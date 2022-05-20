b0bf315535aee480505ef7b4fb52179831ac594c
identityapproved <identityapproved@gmail.com>
2022-05-20 21:56:19 +0300

    click on cell and highlight

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App = () => {
   function restart() {
     const newBoard = new Board();
     newBoard.initCells();
+    newBoard.addFigure();
     setBoard(newBoard);
   }
 
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,12 +3,21 @@ import { Cell } from '../models/Cells';
 
 interface CellProps {
    cell: Cell;
+   selected: boolean;
+   click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({ cell }) => {
+const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
    return (
-      <div className={['cell', cell.color].join(' ')}>
-
+      <div
+         className={['cell', cell.color, selected ? 'selected' : '', /* cell.available && cell.figure ? 'highlight' : '' */].join(' ')
+         }
+         onClick={() => click(cell)}
+         style={{ background: cell.available && cell.figure ? 'green' : '' }}
+      >
+         {cell.available && !cell.figure && <div className={'available'} />}
+         {cell.figure?.logo && <img src={cell.figure.logo} alt='figure' />
+         }
       </div >
    );
 };
